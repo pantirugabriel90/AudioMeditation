00077f369f3c7823be77dd2991e375c9bd342465
alina <alinaharas99@gmail.com>
2024-01-08 14:15:50 +0200

    2 bottoms

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import HomeScreen from "./components/Home"; // Import your Home component
 
 export default function App() {
-  
-const AppContainer = createAppContainer(TabNavigator);
-return <AppContainer />;
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>Open up App.js to start working on your app!</Text>
-  //     <StatusBar style="auto" />
-  //   </View>
-  // );
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
