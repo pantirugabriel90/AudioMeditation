315c3143223ed32dafc8c576dacf8f259842b349
alina <alinaharas99@gmail.com>
2024-06-23 08:30:02 +0300

    remove gradle

--- components/multilanguage/languageService.js
+++ components/multilanguage/languageService.js
@@ -21,7 +21,7 @@ export async function initLocalization() {
   return currentLanguage;
 }
 
-let currentLanguage = "en";
+let currentLanguage = "ro";
 
 export function setLanguage(language) {
   currentLanguage = language;
